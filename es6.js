c8f4368c449471c2fd690592cd17de3196e4cec7
rochisna <rochisnagopireddy>
2025-01-04 11:39:22 +0530

    first version

--- es6.js
+++ es6.js
@@ -9,5 +9,26 @@ console.log(c);
 c = 20;
 console.log(c);
 
-console.log(c);
+//console.log(c);
 //rochisna
+for(let i=1;i<=5;i++){
+    setTimeout(function(){
+        console.log(i);
+    },10);
+}
+for(var i=1;i<=5;i++){
+    setTimeout(function(){
+        console.log(i);
+    },10);
+}
+let person = {
+    fname : "John",
+    lname : "doe",
+}
+person.age = 20;
+console.log("fname"+person.fname);
+console.log("lname"+person.lname);
+console.log("age"+person.age);
+
+
+
